b9e26ae1d8fa3290806e49d72ba856308defe7f0
tarosjk <tarosjk@gmail.com>
2024-06-03 14:42:21 +0900

    Lesson 4まで完了

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// $(document).ready(function () {
+//   alert('jQuery動作テスト');
+// });
+
+// document.querySelector('h1').style.color = 'green'
+
+$('h1').css('color', 'green')
+$('.site-header-nav li a ').css('color', 'blue')
+
+$('.mv-copy').next().css('text-decoration', 'underline')
+
+$('.works-panel').find('.works-title').css('border-bottom', '2px solid #000')
