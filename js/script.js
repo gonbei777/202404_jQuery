64f8fb13e20fd61d2b2197636865513edafe2d9f
tarosjk <tarosjk@gmail.com>
2024-06-07 09:50:50 +0900

    ハンバーガーメニューの実装完了

--- js/script.js
+++ js/script.js
@@ -283,3 +283,11 @@ $(window).on('load', () => {
   )
 
 })
+
+$('.site-header-navbtn').on('click', () => {
+  $('body').toggleClass('is-nav-open')
+})
+
+$('.site-header-nav').on('click', 'a', () => {
+  $('body').removeClass('is-nav-open')
+})
